a3bf0898ea0baeb0c3379da39a4c073bdb008625
Mark Underseth <marku@s2technologies.com>
2017-03-14 15:42:37 +0000

    latest client

--- c9build.sh
+++ c9build.sh
@@ -3,7 +3,7 @@
 # Build
 
 # Download Client
-curl -s https://testspace-client.s3.amazonaws.com/testspace-linux-dev.tgz | sudo tar -zxvf- -C /usr/local/bin
+curl -s https://testspace-client.s3.amazonaws.com/testspace-linux-daily.tgz | sudo tar -zxvf- -C /usr/local/bin
 
 # mark-s2-tec
 export MARK_TOKEN=095d8acfaaf71b4a0f3767e41152620ebdda1b31
