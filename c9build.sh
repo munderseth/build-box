7f15379701b9edde840dd8a836bbc7d50b2e1bee
Mark Underseth <marku@s2technologies.com>
2017-03-14 16:42:59 +0000

    organize

--- c9build.sh
+++ c9build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Build
-
 # Download Client
 curl -s https://testspace-client.s3.amazonaws.com/testspace-linux-daily.tgz | sudo tar -zxvf- -C /usr/local/bin
 
-# mark-s2-tec
+# Build
+
+# Test
+
+# Post (using "mark-s2-tec")
 export MARK_TOKEN=095d8acfaaf71b4a0f3767e41152620ebdda1b31
 testspace config url http://$MARK_TOKEN@mark.stridespace.com/munderseth:build-box
 CI=true testspace test.xml
