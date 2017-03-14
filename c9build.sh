2b553ef6c33e7ceee691144bf49ef2c03efbf04e
Mark Underseth <marku@s2technologies.com>
2017-03-14 17:02:53 +0000

    annotate env vars

--- c9build.sh
+++ c9build.sh
@@ -10,4 +10,5 @@ curl -s https://testspace-client.s3.amazonaws.com/testspace-linux-daily.tgz | su
 # Post (using "mark-s2-tec")
 export MARK_TOKEN=095d8acfaaf71b4a0f3767e41152620ebdda1b31
 testspace config url http://$MARK_TOKEN@mark.stridespace.com/munderseth:build-box
-CI=true testspace test.xml
+printenv > printenv.txt
+CI=true testspace test.xml "+printenv.txt{environment variables}"
