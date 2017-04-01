889ecdf810c265b224539c1e3b64c6c5997c9a0a
Mark Underseth <marku@s2technologies.com>
2017-04-01 13:51:11 +0000

    config space

--- c9build.sh
+++ c9build.sh
@@ -10,6 +10,7 @@ curl -s https://testspace-client.s3.amazonaws.com/testspace-linux-dev.tgz | sudo
 
 # Post (using "mark-s2-tec")
 export MARK_TOKEN=095d8acfaaf71b4a0f3767e41152620ebdda1b31
-CI=true testspace config url http://$MARK_TOKEN@mark.stridespace.com/munderseth:build-box
+BRANCH_NAME=`git symbolic-ref --short HEAD`
+testspace config url http://$MARK_TOKEN@mark.stridespace.com/munderseth:build-box/${BRANCH_NAME}
 printenv > printenv.txt
-testspace test.xml "+printenv.txt{environment variables}"
+testspace test.xml "+printenv.txt{environment variables}" "#build.c9"
